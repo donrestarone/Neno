0d6b8dc6eefb5bf9110bcb1365d1c18ebfecb381
donrestarone <shashikejayatunge@gmail.com>
2018-05-21 17:37:56 -0400

    authentication works

--- neno/app/controllers/application_controller.rb
+++ neno/app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   		@current_user ||= User.find(session[:user_id])
   	end
   end
+
+  def user_already_logged_in
+  	if session[:user_id] != nil
+  		redirect_to root_path
+  	end
+  end
 end
